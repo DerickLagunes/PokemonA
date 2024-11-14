536331cef137d27f58e0bdd973d80327f2feca90
Derick Lagunes <dericklagunes@utez.edu.mx>
2024-11-14 12:30:41 -0600

    algo

---
+++ Batalla.java
@@ -0,0 +1,10 @@
+public class Batalla {
+    //Que deben de implementar la clase Queue.java
+    //Que hicimos en clase (osea no la de util.java.Queue)
+
+    //Hacer una batlla por turnos donde en consola se pida el ataque a realizar
+    //(Scanner) y hacewr la pila de turnos
+    //y despues de interactuar ambos pokemon
+    //seguir peleando si aun hay uno en el equipo o tenerminar el juego
+    //si todos lo spokemon del equipo tiene vida <0=0
+}
